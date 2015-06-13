608ba005d60588a2b3118aebf0649e4b3bafced0
Balazs Banyai <balazs.banyai@logmein.com>
2015-06-13 13:31:22 +0200

    added app icon

--- app/src/main/java/com/banyaibalazs/batterysaverlollipop/MainActivity.java
+++ app/src/main/java/com/banyaibalazs/batterysaverlollipop/MainActivity.java
@@ -16,25 +16,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         findViewById(R.id.navigateToAccessibilitySettings).setOnClickListener(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
